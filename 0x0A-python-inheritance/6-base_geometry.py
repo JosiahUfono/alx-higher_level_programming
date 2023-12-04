8c3143f292292f444971308206e4b5538c40594e
Josiah Ufono <josiahufono@gmail.com>
2023-12-04 23:10:14 +0000

    Task 6:  a class BaseGeometry (based on 5-base_geometry.py)

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Defines class BaseGeometry.'''
+
+
+class BaseGeometry:
+    '''Repremsets class BaseGeometry.'''
+
+    def area(self):
+        '''A function which raises an exception'''
+        raise Exception("area() is not implemented")
